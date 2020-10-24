9cf569f0f39c176ce45359b266722c301df80450
introspecting2031 <introspecting2031@outlook.com>
2020-10-24 23:16:10 +0800

    关闭CPU数据缓存以免NVDLA模块写入的数据没有被同步。

--- run_trace_tests.c
+++ run_trace_tests.c
@@ -1,7 +1,7 @@
 #include "nvdla_verif.h"
 #include "nvdla_registers.h"
 #include <stdio.h>
-
+#include <xil_cache.h>
 /**
  * 启用某个测试，如pdp_8x9x19_3x3_ave_int8_0。
  * 在nvdla_verif.c定义#define pdp_8x9x19_3x3_ave_int8_0 1
@@ -14,8 +14,10 @@
 
 int run_trace_tests(void)
 {
+	Xil_DCacheDisable();
 
-#include "nv_small/pdp_8x9x19_3x3_ave_int8_0/pdp_8x9x19_3x3_ave_int8_0.cfg"
+
+#include "data/nv_small/pdp_8x9x19_3x3_ave_int8_0/pdp_8x9x19_3x3_ave_int8_0.cfg"
 
     fprintf(stderr, "test passed");
     
